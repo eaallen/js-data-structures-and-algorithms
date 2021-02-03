004608d7213f9479cd8c30a48a36bf35c4f9a31d
eaallen <elijah.a.allen@gmail.com>
2021-02-02 19:46:45 -0700

    FakeArray

---
+++ hw/html/array/Array.js
@@ -0,0 +1,88 @@
+//------------------------------------------------
+// Elijah Allen 11am section  
+//-----------------------------------------------
+
+// Good 'ol stack overflow
+String.prototype.insert = function (index, string) {
+    if (index > 0) {
+        return this.substring(0, index) + string + this.substring(index);
+    } return string + this;
+};
+
+class FakeArray {
+    constructor() {
+        this.contents = ''
+        this.length = this.contents.length
+    }
+
+    push(byte4) {
+        byte4 = byte4.toString()
+        
+        // array is at max     input is non number  input is float
+        if (this.length >= 40 || isNaN(byte4) || byte4.includes('.')) {
+            return null
+        }
+
+        while (byte4.length < 4) {
+            if (byte4[0] === '-') {
+                byte4 = byte4.insert(1, '0')
+            } else {
+                byte4 = '0' + byte4
+            }
+        }
+
+        this.contents += byte4
+        this.length = this.contents.length
+
+        return true
+    }
+
+    pop() {
+        // array is empty
+        if (this.length < 4) {
+            return null
+        }
+
+        let popped = ''
+        for (let i = this.length - 1; i > this.length - 5; i--) {
+            popped = this.contents[i] + popped
+        }
+
+        this.contents = this.contents.substring(0, this.length - 4)
+        this.length = this.contents.length
+
+        return popped
+    }
+
+    get(index) {
+        if (index < 0 || index * 4 > this.length - 4) {
+            return null
+        }
+
+        return parseInt(this.contents.substring(index * 4, (index * 4) + 4))
+    }
+
+}
+
+const array = new FakeArray()
+
+
+// for (let i = 0; i < 10; i++) {
+//     array.push(12)
+// }
+array.push(1)
+array.push(2)
+array.push(-3)
+array.push(4.1)
+
+console.log(array.contents)
+console.log(array.get(0))
+console.log(array.get(1))
+console.log(array.get(2))
+console.log(array.get(3))
+
+console.log('-------------------')
+
+
+console.log(array.get(3))
+
---
+++ hw/html/experiment.js
@@ -0,0 +1,5 @@
+// I thought it would be fun and insightful to create a little program 
+// that does some kind of big loop such that it could test the speed of
+// 1. a pure character compare
+// 2. a string compare
+// 3. a regex compare
