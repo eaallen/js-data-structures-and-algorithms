fd502fe76c2672cd172af57fb4ce5dd65cee0e61
eaallen <elijah.a.allen@gmail.com>
2021-02-02 23:17:16 -0700

    .

---
+++ hw/html/search_and_sort/index.js
@@ -0,0 +1,19 @@
+// Binary search is faster that Linear search 
+function binarySearch(array, n) {
+    var lowIndex = 0, highIndex = array1.length - 1;
+
+    while (lowIndex <= highIndex) {
+        // first: midIndex = (array.length -1) / 2
+        var midIndex = Math.floor((highIndex + lowIndex) / 2);
+        if (array[midIndex] == n) {
+            return midIndex;
+        } // grab the higher half 
+        else if (n > array[midIndex]) {
+            lowIndex = midIndex;
+        }  // grab the lower half   
+        else {
+            highIndex = midIndex;
+        }
+    }
+    return -1;
+}
