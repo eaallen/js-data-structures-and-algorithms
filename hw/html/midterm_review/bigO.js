e69fe85f33b41ccc42686b83197370d7b442591d
eaallen <elijah.a.allen@gmail.com>
2021-02-24 22:07:40 -0700

    midtermReview

---
+++ hw/html/midterm_review/bigO.js
@@ -0,0 +1,21 @@
+// Sum Rule
+function sum(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i)
+    }
+    for (let i = 0; i < n; i++) {
+        console.log(i)
+    }
+}
+
+// Product Rule
+function forloop(n) {
+    for (let i = 0; i < n * 3; i++) {
+        debug(n)
+    }
+}
+function debug(n) {
+    for (let i = 0; i < n; i++) {
+        console.log('debuged')
+    }
+}
