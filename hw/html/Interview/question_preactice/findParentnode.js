3667f0972f79c6c084845099eabeae701cf7f257
eaallen <elijah.a.allen@gmail.com>
2021-04-13 15:32:29 -0600

    .

--- hw/html/Interview/question_preactice/findParentnode.js
+++ hw/html/Interview/question_preactice/findParentnode.js
@@ -1,4 +1,3 @@
-// build binary tree
 class AllenTree {
   constructor(value) {
     this.root = null
@@ -6,38 +5,41 @@ class AllenTree {
   }
 
   createNode(value) {
-    let height = null
+    let depth = null
+    if (tree) {
+      // find depth 
+      // this.getDepth()
+    }
     return {
       value: value,
       left: null,
       right: null,
-      height: height,
-      depth: null,
+      depth: depth,
     }
   }
 
-  getHeight = (root) => {
-    let height = 0
+  getDepth(root) {
+    let depth = 0
     if (root) {
-      height = Math.max(this.getHeight(root.left), this.getHeight(root.right)) + 1
-      root.height = height - 1
+      depth = Math.max(this.getDepth(root.left), this.getDepth(root.right)) + 1
+      root.depth = depth
     }
-    return height
+    return depth
   }
   search(value) {
     const searchHelper = (node, value) => {
-      if (node.value === value || node.value === null) {
+      if (node.value === value) {
         return node.count
       } else if (node.value > value) {
         searchHelper(node.left, value)
       } else {
         searchHelper(node.right, value)
       }
+
     }
     return searchHelper(this.root, value)
   }
 
-  // insert will not self ballence
   insert(value) {
     const _insert = (node, value) => {
       if (this.root === null) {
@@ -48,6 +50,7 @@ class AllenTree {
       } else if (value < node.value) {
         // go Left
         // check for balence
+        console.log(this.nodeIsBalenced(node))
         node.is_balenced = this.nodeIsBalenced(node)
         if (node.left === null) {
           node.left = this.createNode(value)
@@ -58,6 +61,7 @@ class AllenTree {
       } else if (value > node.value) {
         // go right
         // check for balence
+        console.log(this.nodeIsBalenced(node))
         node.is_balenced = this.nodeIsBalenced(node)
         if (node.right === null) {
           node.right = this.createNode(value)
@@ -74,6 +78,7 @@ class AllenTree {
   }
 
   add(data) {
+    console.log('ADD:', data)
     const _insert = (root, node) => {
       // if(node.value===70) debugger
       if (!root) {
@@ -83,9 +88,12 @@ class AllenTree {
         root.left = _insert(root.left, node)
         if (root.left !== null && !this.nodeIsBalenced(root)) {
           // rotation
+          console.log('left rotate')
           if (root.left.value > node.value) { // node is on left 
+            console.log('RotateLL')
             root = this.rotateLL(root)
           } else {
+            console.log('rotateLR')
             root = this.rotateLR(root)
           }
         }
@@ -94,9 +102,14 @@ class AllenTree {
         root.right = _insert(root.right, node)
         if (root.right !== null && !this.nodeIsBalenced(root)) {
           // rotation 
+          console.log('right rotate! Root.right:', root.right)
+          console.log('right rotate! NODE:', node)
+
           if (root.right.value < node.value) { // node is on left 
+            console.log('RotateRR')
             root = this.rotateRR(root)
           } else {
+            console.log('rotateRL')
             root = this.rotateRL(root)
           }
         }
@@ -113,6 +126,7 @@ class AllenTree {
     return
   }
   rotateLL(root) {
+    console.log('LL:', root)
     const z = root
     const y = root.left
     const x = root.left.left
@@ -122,6 +136,7 @@ class AllenTree {
     y.left = x
     y.right = z
 
+    console.log('LL 2:', y)
     return y
   }
 
@@ -140,6 +155,7 @@ class AllenTree {
 
 
   rotateRR(root) {
+    console.log('RR:', root)
     const z = root
     const y = root.right
     const x = root.right.right
@@ -150,6 +166,7 @@ class AllenTree {
     y.right = x
     y.left = z
 
+    console.log('RR 2:', y)
     return y
   }
   rotateRL(root) {
@@ -168,7 +185,7 @@ class AllenTree {
   inspect() { console.log(this) }
 
   getTreeHeight() {
-    return this.getHeight(this.root)
+    return this.getDepth(this.root)
   }
   /**
    * returns true if the differance of depth in 
@@ -177,7 +194,7 @@ class AllenTree {
    * @returns {true}
    */
   nodeIsBalenced(node) {
-    return Math.abs(this.getHeight(node.left) - this.getHeight(node.right)) < 2
+    return Math.abs(this.getDepth(node.left) - this.getDepth(node.right)) < 2
   }
 
   traverse(root = this.root, valuesOnly = true) {
@@ -199,80 +216,29 @@ class AllenTree {
     return arr
   }
 
-  findNodesAtKDepthMaster(k) {
-    let trav_count = 0
-    const arr = []
-    const traverse = (node) => {
-      if (node.left && trav_count < k) {
-        trav_count++
-        traverse(node.left)
-      }
-      if (node.right && trav_count < k) {
-        trav_count++
-        traverse(node.right)
-      }
-      if (trav_count >= k) {
-        arr.push(node.value)
-      }
-      trav_count--
-    }
-    traverse(this.root)
-    return arr
-  }
-
-  // overrider
-  findNodesAtKDepth(callback) {
-  }
-
-  populate(n = 50) {
-    for (let i = 1; i < n; i++) {
-      this.add(i)
-    }
-  }
-
-  test(callback, dom_id) {
-    const k = 3
-    const s = ', '
-    const your_array = callback(k, this.root)
-    const my_array = this.findNodesAtKDepthMaster(k)
-    let message = `<p style='color:green;'>success, your array: ${your_array.join(s)} <br> is the same as my array ${my_array.join(s)}</p>`
-
-    for (const item of my_array) {
-      if (!your_array.includes(item)) {
-        message = `<p style='color:red;'>falure, your array: ${your_array.join(s)} <br> is not the same as my array ${my_array.join(s)}</p>`
-        break
-      }
-    }
-    document.getElementById(dom_id).innerHTML = message
-  }
-
   findNodeParents(value) {
     const parent_nodes = []
     const find = (node) => {
-      if (!node || node.value === value) {
+      parent_nodes.push(node)
+      if(node.value === value || !node){
         // found node
         return node
       }
-      if (node.value > value) {
+      if(node.value < value){
         // go left
-        parent_nodes.push(node)
         find(node.left)
-      } else if (node.value < value) {
+      }else if(node.value < value){
         // go right
-        parent_nodes.push(node)
         find(node.right)
       }
+
     }
 
     find(this.root)
     console.log(parent_nodes)
-    return parent_nodes
   }
+
 }
 
 
-const tree = new AllenTree(0)
-tree.populate(50)
-console.log(tree)
-// console.log(tree.findNodesAtKDepthMaster(3))
-tree.findNodeParents(25)
+
